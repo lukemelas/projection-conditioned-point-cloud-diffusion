64fd55a0d00b52735cf02e11c5112374c7104ece
Luke <luke.melas@gmail.com>
2023-04-11 17:41:09 -0700

    Update download scripts

--- experiments/scripts/download-example-logs-and-checkpoints.sh
+++ experiments/scripts/download-example-logs-and-checkpoints.sh
@@ -1,1 +1,3 @@
-echo "Checkpoints will be uploaded soon!"
+# Download and unzip
+wget https://github.com/lukemelas/projection-conditioned-point-cloud-diffusion/releases/download/v0.0/example-logs-and-checkpoints.zip
+unzip example-logs-and-checkpoints.zip && rm example-logs-and-checkpoints.zip
--- experiments/scripts/download-example-vis.sh
+++ experiments/scripts/download-example-vis.sh
@@ -1,1 +1,5 @@
-echo "Checkpoints will be uploaded soon!"
+# Download files (the zip file is split into two files) and then unzip
+wget https://github.com/lukemelas/projection-conditioned-point-cloud-diffusion/releases/download/v0.0/example-vis-parts.zip
+wget https://github.com/lukemelas/projection-conditioned-point-cloud-diffusion/releases/download/v0.0/example-vis-parts.z01
+zip -FF example-vis-parts.zip --out example-vis.zip && rm example-vis-parts.zip example-vis-parts.z01
+unzip example-vis.zip && rm example-vis.zip
